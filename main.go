4f3fc246769e08e9c7a79b2402d1b38a19c4b89c
ronardcaktus <rluna@caktusgroup.com>
2024-08-16 09:00:32 -0400

    Enable search functionality

--- main.go
+++ main.go
@@ -94,8 +94,21 @@ func handleFunc(db *sql.DB, opts []wmenu.Opt) {
 		addProvince(db, newProvince)
 
 		break
+
 	case 1:
-		fmt.Println("Finding a Province")
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter a province's name or capital to search by: ")
+		searchString, _ := reader.ReadString('\n')
+		searchString = strings.TrimSuffix(searchString, "\n")
+		province := searchProvince(db, searchString)
+
+		fmt.Printf("Found %v results\n", len(province))
+
+		for _, foundProvince := range province {
+			fmt.Printf("\nID: %d\n------\nProvince: %s\nCapital: %s\nRegion: %s\nDepartment: %s\nArea(km2): %s\nPopulation(2021): %s\nDensity: %s\nDepartment: %s\n", foundProvince.id, foundProvince.province, foundProvince.capital, foundProvince.region, foundProvince.department, foundProvince.area_km2, foundProvince.population_2021, foundProvince.density, foundProvince.established_year)
+		}
+		break
+
 	case 2:
 		fmt.Println("Update a Province's information")
 	case 3:
--- models.go
+++ models.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -25,5 +26,34 @@ func addProvince(db *sql.DB, newProvince province) {
 	stmt.Exec(nil, newProvince.province, newProvince.capital, newProvince.region, newProvince.department, newProvince.area_km2, newProvince.population_2021, newProvince.density, newProvince.established_year)
 	defer stmt.Close()
 
-	fmt.Printf("The province of %v with an ID %v was just added. \n", newProvince.province, newProvince.id)
+	fmt.Printf("The province of %v was just added. \n", newProvince.province)
+}
+
+func searchProvince(db *sql.DB, searchString string) []province {
+	rows, err := db.Query("SELECT id, province, capital, region, department, area_km2, population_2021, density, established_year FROM provinces WHERE province like '%" + searchString + "%' OR capital like '%" + searchString + "%'")
+	defer rows.Close()
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	provinces := make([]province, 0)
+
+	for rows.Next() {
+		foundProvince := province{}
+		err = rows.Scan(&foundProvince.id, &foundProvince.province, &foundProvince.capital, &foundProvince.region, &foundProvince.department, &foundProvince.area_km2, &foundProvince.population_2021, &foundProvince.density, &foundProvince.established_year)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		provinces = append(provinces, foundProvince)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return provinces
 }

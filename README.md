9995f3795c49f17bc87d55ae4772e31a20094ca1
ronardcaktus <rluna@caktusgroup.com>
2024-08-18 16:10:10 -0400

    Create command to import data, create directories, and README

---
+++ README.md
@@ -0,0 +1,27 @@
+# Quisqueya Project
+
+Quisqueya is a Taíno word that means "mother of the earth". It's the indigenous name for the island of Hispaniola, 
+which is now made up of Haiti and the [Dominican Republic](https://en.wikipedia.org/wiki/Dominican_Republic). I created 
+this project as a way to practice and improve my `Go` skills. Currently it is a CRUD command line application that connects to sqlite3. 
+I will slowly but steadily add new features to the repo, once I decide what those will be 😅.
+
+## Develop
+Go >= 1.2x
+Sqlite3 >= 3.x
+
+## Create DB and import data
+
+There is a CSV that contains some initial data about provinces in the the Dominican Republic. To creare a DB and populate 
+it with data, do the following:
+
+```shell
+cd cmd/importer
+go build importer.go 
+./importer -csv province_data.csv -db quisqueya.db 
+```
+
+move that `quisqueya.db` back into the top-level directory.
+
+*Note*: Running this command implies that you are starting a DB from scratch, it should not be run if data already exists. 
+
+
---
+++ cmd/importer/importer.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/csv"
+	"errors"
+	"flag"
+	"io"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	var dbName string
+	var csvName string
+	flag.StringVar(&dbName, "db", "", "SQLite database to import to")
+	flag.StringVar(&csvName, "csv", "", "CSV file to import from")
+	flag.Parse()
+
+	if dbName == "" || csvName == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	// Open DB
+	db, err := sql.Open("sqlite3", dbName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("ping failed: %s", err)
+	}
+	stmt, err := db.Prepare("CREATE TABLE IF NOT EXISTS provinces (id INTEGER PRIMARY KEY AUTOINCREMENT, province TEXT, capital TEXT, region TEXT, department TEXT, area_km2 TEXT, population_2021 TEXT, density TEXT, established_year TEXT)")
+	if err != nil {
+		log.Fatalf("prepare failed: %s", err)
+	}
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Fatalf("exec failed: %s", err)
+	}
+
+	// Open CSV file
+	f, err := os.Open(csvName)
+	if err != nil {
+		log.Fatalf("open failed: %s", err)
+	}
+	r := csv.NewReader(f)
+	// Read the header row.
+	_, err = r.Read()
+	if err != nil {
+		log.Fatalf("missing header row(?): %s", err)
+	}
+	for {
+		record, err := r.Read()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		province := record[0]
+		capital := record[1]
+		region := record[2]
+		department := record[3]
+		area_km2 := record[4]
+		population_2021 := record[5]
+		density := record[6]
+		established_year := record[7]
+
+		stmt, err = db.Prepare("insert into provinces(province, capital, region, department, area_km2, population_2021, density, established_year) values(?, ?, ?, ?, ?, ?, ?, ?)")
+		if err != nil {
+			log.Fatalf("Insert prepare failed: %s", err)
+		}
+
+		_, err = stmt.Exec(province, capital, region, department, area_km2, population_2021, density, established_year)
+		if err != nil {
+			log.Fatalf("insert failed(%s): %s", province, err)
+		}
+	}
+}
